7e08c67da45178d6e88a625511ea2f43a3a61b19
Rohan Goparaju <goparajurohan41@gmail.com>
2026-07-19 11:25:31 -0400

    Constructed full dataframe from the nested json file

--- preprocessing.py
+++ preprocessing.py
@@ -4,5 +4,12 @@ import json
 def preprocessing(f):
     with open(f) as file:
         data = json.load(file)
-    df = pd.json_normalize(data['rounds'])
-    print(df.columns)
+    df_round = pd.json_normalize(data['rounds'])
+    communications = []
+    for round in data['rounds']:
+        for msg in round.get('communications',[]):
+            msg['hour'] = round['hour']
+            communications.append(msg)
+    df_communication = pd.json_normalize(communications )
+    df = pd.concat([df_round,df_communication])
+    print(df.dtypes)
